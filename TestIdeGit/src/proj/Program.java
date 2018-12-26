ddab73588b94736a7df3cb97acc0699bb82522af
volodymyr <volodymyr@LenovoUbuntu>
2018-12-26 15:58:44 +0200

    Comit2
    added cycle for

--- TestIdeGit/src/proj/Program.java
+++ TestIdeGit/src/proj/Program.java
@@ -3,9 +3,10 @@ package proj;
 public class Program {
 
 	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println("Hello World!");
 
-		System.out.println("Hello World!");
+		}
 
 	}
-
 }
